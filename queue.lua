fe5e73b5a995f892a0341baf6df6037c2db03455
hong <hongling-0@qq.com>
2019-05-10 11:30:28 +0800

    circ_queue.lua

--- queue.lua
+++ queue.lua
@@ -1,38 +1,64 @@
-local queue=require 'queue.c'
+return function()
+	local read=1
+	local write=1
+	local cap=64
+	local q={}
+	local function size()
+		local sz=write-read
+		if sz>=0 then return sz,cap end
+		return cap+sz,cap
+	end
+	local function pop()
+		local val
+		if read==write then return end
+		val,q[read]=q[read],nil
+		read=read+1
+		if read>cap then read=1 end
+		return val
+	end
+	local function top()
+		if read==write then return end
+		local val=q[read]
+		return val
+	end
+	local function push(val)
+		q[write]=val
+		write=write+1
+		if write>cap then write=1 end
+		if write==read then
+			local oq=q
+			local owrite=write
+			local oread=read
+			local ocap=cap
 
-for k,v in pairs(queue) do
-    print(k,v)
+			read=1
+			write=1
+			cap=ocap*2
+			q={}
+			for i=oread,ocap do
+				push(oq[i])
+			end
+			for i=1,owrite-1 do
+				push(oq[i])
+			end
+		end
+	end
+	local function pairs()
+		local i=0
+		local sz=size()
+		return function()
+			if i==sz then return end
+			local idx=i+read
+			if idx>cap then idx=idx-cap end
+			i=i+1
+			return i,q[idx]
+		end
+	end
+	return setmetatable({
+		pop=pop,
+		push=push,
+		size=size,
+		top=top,
+		--table=function() return q end,
+	},{__pairs=pairs})
 end
-
-local q=queue.new(100)
-print('size',q.size())
-for i=1,2500000 do
-    q.push(i)
-end
-print('size',q.size())
-
-
-for i=1,2500000 do
-    local ok,val=q.pop()
-    assert(ok)
-end
-print('size',q.size())
-
-for i=1,2500000 do
-    q.push(nil)
-end
-print('size',q.size())
-
-
-for i=1,2500000 do
-    local ok,val=q.pop()
-    q.push(i)
-    q.push(i)
-    assert(ok)
-end
-print('size',q.size())
-local i=0
-for k,v in pairs(q.table()) do
-    i=i+1
-end
-print('head',i,q.head())
