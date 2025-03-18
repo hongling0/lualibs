2be927d87b906cd93912e631378506b40e4fe8a6
hong <hongling0@gmail.com>
2025-03-18 16:04:16 +0800

    fix #2

--- README.md
+++ README.md
@@ -7,28 +7,23 @@
 
 
 # lua_queue
-lua fifo queue. can replace table.insert(t) table.remove((t,1)
+lua fifo queue. can replace table.insert(t) table.remove(t,1)
   
-    local queue=require 'queue.c'
-    local q=queue()
+    local q=require 'queue'()
     for i=1,100000 do
         q.push(i)
+        if i % 2 == 1 then
+            print(q.pop())
+        end
     end
-    for i=1,100000 do
-        local ok,val=q.pop()
-        assert(ok)
-    end
-    local ok,val=q.head()
-    for i,v in pairs(q) do
-      print(i,v)
-    end
+    print(q.top(), #q)
 
 
 # lua string split
     
     local split=require 'split.c'
-    print(split.splitrow(",asd,asd,,,asdasd,asd,,,",",")
-    print(split.splitrow(",asd,asd,,,asdasd,asd,,,",",","i")
+    print(split.splitrow(",asd,asd,,,asdasd,asd,,,",","))
+    print(split.splitrow(",asd,asd,,,asdasd,asd,,,",",","i"))
     print('split table-------------------1')
     for i,v in ipairs(split.split(",asd,asd,,,asdasd,asd,,,",",")) do print(k,v) end
     print('split table-------------------2')
--- queue.lua
+++ queue.lua
@@ -1,57 +1,43 @@
 return function()
-	local read,write,cap=1,1,64
-	local q={}
+	local q, cap, read, write = {}, 2, 1, 1
 	local function size()
-		local sz=write-read
-		if sz>=0 then return sz,cap end
-		return cap+sz,cap
+		local sz = write - read
+		if sz >= 0 then return sz, cap end
+		return cap + sz, cap
 	end
 	local function pop()
 		local val
-		if read==write then return end
-		val,q[read]=q[read],nil
-		read=read+1
-		if read>cap then read=1 end
+		if read == write then return end
+		val, q[read] = q[read], nil
+		read = read + 1
+		if read > cap then read = 1 end
 		return val
 	end
 	local function top()
-		if read==write then return end
-		local val=q[read]
+		if read == write then return end
+		local val = q[read]
 		return val
 	end
 	local function push(val)
-		q[write]=val
-		write=write+1
-		if write>cap then write=1 end
-		if write==read then
-			local oq=q
-			local oread,owrite,ocap=read,write,cap
-			read,write,cap=1,1,ocap*2
-			q={}
-			for i=oread,ocap do
-				push(oq[i])
-			end
-			for i=1,owrite-1 do
-				push(oq[i])
-			end
+		q[write] = val
+		write = write + 1
+		if write > cap then write = 1 end
+		if write == read then
+			for i = 1, write - 1 do q[cap + i], q[i] = q[i], nil end
+			cap, write = cap * 2, cap + write
 		end
 	end
 	local function pairs()
-		local i=0
-		local sz=size()
+		local i = 0
+		local sz = size()
 		return function()
-			if i==sz then return end
-			local idx=i+read
-			if idx>cap then idx=idx-cap end
-			i=i+1
-			return i,q[idx]
+			if i == sz then return end
+			local idx = i + read
+			if idx > cap then idx = idx - cap end
+			i = i + 1
+			return i, q[idx]
 		end
 	end
-	return setmetatable({
-		pop=pop,
-		push=push,
-		size=size,
-		top=top,
-		--table=function() return q end,
-	},{__pairs=pairs})
+	return setmetatable({ pop = pop, push = push, size = size, top = top },
+		{ __pairs = pairs, __len = size })
 end
